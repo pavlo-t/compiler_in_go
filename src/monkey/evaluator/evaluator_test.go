546375b3bad9bf6ed70353f63e7d7e5ac7a44bbb
Pavlo Tsapliienko <pavel.tsaplienko@gmail.com>
2025-09-25 22:17:09 +0300

    eval: bindings and the environment

--- src/monkey/evaluator/evaluator_test.go
+++ src/monkey/evaluator/evaluator_test.go
@@ -152,10 +152,7 @@ if (10 > 1) {
     return true + false;
   }
   return 1;
-}
-`,
-			"unknown operator: BOOLEAN + BOOLEAN",
-		},
+}`, "unknown operator: BOOLEAN + BOOLEAN"},
 		{`
 if (10 > 1) {
   if (10 > 1) {
@@ -163,15 +160,13 @@ if (10 > 1) {
     return 2;
   }
   return 1;
-}
-`,
-			"unknown operator: BOOLEAN + BOOLEAN",
-		},
+}`, "unknown operator: BOOLEAN + BOOLEAN"},
 		{"return true + false; return 12;", "unknown operator: BOOLEAN + BOOLEAN"},
 		{"-(true + false)", "unknown operator: BOOLEAN + BOOLEAN"},
 		{"(true + false) - (1 + 1)", "unknown operator: BOOLEAN + BOOLEAN"},
 		{"(1 + 1) - (true + false)", "unknown operator: BOOLEAN + BOOLEAN"},
 		{"if (true + false) { return 10; }", "unknown operator: BOOLEAN + BOOLEAN"},
+		{"foobar", "identifier not found: foobar"},
 	}
 
 	for _, tt := range tests {
@@ -191,12 +186,29 @@ if (10 > 1) {
 	}
 }
 
+func TestLetStatements(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let a = 5; a;", 5},
+		{"let a = 5 * 5; a;", 25},
+		{"let a = 5; let b = a; b;", 5},
+		{"let a = 5; let b = a; let c = a + b + 5; c;", 15},
+	}
+
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
+	}
+}
+
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
 	program := p.ParseProgram()
+	env := object.NewEnvironment()
 
-	return Eval(program)
+	return Eval(program, env)
 }
 
 func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
