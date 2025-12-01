ac1e684b0cf52147ccdf0e29ac9eb054a246e5ec
Pavlo Tsapliienko <pavel.tsaplienko@gmail.com>
2025-12-01 20:28:28 +0200

    macro: unquote booleans, strings, quoted code

--- src/monkey/evaluator/quote_unquote_test.go
+++ src/monkey/evaluator/quote_unquote_test.go
@@ -42,20 +42,24 @@ func TestQuoteUnquote(t *testing.T) {
 		{`quote(unquote(4 + 4) + 8)`, `(8 + 8)`},
 		{`let foobar = 8; quote(foobar)`, `foobar`},
 		{`let foobar = 8; quote(unquote(foobar))`, `8`},
+		{`quote(unquote(true))`, `true`},
+		{`quote(unquote(true == false))`, `false`},
+		{`quote(unquote("s"))`, `s`},
+		{`quote(unquote("Hello " + "World!"))`, `Hello World!`},
+		{`quote(unquote(quote(4 + 4)))`, `(4 + 4)`},
+		{`let quotedInfixExpression = quote(4 + 4); quote(unquote(4 + 4) + unquote(quotedInfixExpression))`, `(8 + (4 + 4))`},
 	}
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
 		quote, ok := evaluated.(*object.Quote)
 		if !ok {
-			t.Fatalf("expected *object.Quote. got=%T (%+v)",
-				evaluated, evaluated)
+			t.Fatalf("expected *object.Quote. got=%T (%+v)", evaluated, evaluated)
 		}
 		if quote.Node == nil {
-			t.Fatalf("quote.Node is nil")
+			t.Fatalf("quote.Node is nil, want=%q", tt.expected)
 		}
 		if quote.Node.String() != tt.expected {
-			t.Errorf("not equal. got=%q, want=%q",
-				quote.Node.String(), tt.expected)
+			t.Errorf("not equal. got=%q, want=%q", quote.Node.String(), tt.expected)
 		}
 	}
 }
