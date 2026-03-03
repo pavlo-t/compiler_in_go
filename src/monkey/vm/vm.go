df16dbe6966de0221e216d0ac02a28c9eee08a79
Pavlo Tsapliienko <pavel.tsaplienko@gmail.com>
2026-03-03 19:23:09 +0200

    OpEqual, OpNotEqual, OpGreaterThan

--- src/monkey/vm/vm.go
+++ src/monkey/vm/vm.go
@@ -61,6 +61,11 @@ func (vm *VM) Run() error {
 			if err != nil {
 				return err
 			}
+		case code.OpEqual, code.OpNotEqual, code.OpGreaterThan:
+			err := vm.executeComparisonOperation(op)
+			if err != nil {
+				return err
+			}
 		case code.OpPop:
 			vm.pop()
 		}
@@ -70,20 +75,19 @@ func (vm *VM) Run() error {
 }
 
 func (vm *VM) executeBinaryOperation(op code.Opcode) error {
-	r := vm.pop().(*object.Integer)
-	l := vm.pop().(*object.Integer)
+	r := vm.pop()
+	l := vm.pop()
 
-	leftType := l.Type()
-	rightType := r.Type()
-
-	if leftType == object.INTEGER_OBJ && rightType == object.INTEGER_OBJ {
-		return vm.executeBinaryIntegerOperation(op, l.Value, r.Value)
+	if l.Type() == object.INTEGER_OBJ && r.Type() == object.INTEGER_OBJ {
+		return vm.executeBinaryIntegerOperation(op, l, r)
 	}
 
-	return fmt.Errorf("unsupported types for binary operation: %s %s", leftType, rightType)
+	return fmt.Errorf("unsupported types for binary operation: %s %s", l.Type(), r.Type())
 }
 
-func (vm *VM) executeBinaryIntegerOperation(op code.Opcode, l int64, r int64) error {
+func (vm *VM) executeBinaryIntegerOperation(op code.Opcode, left, right object.Object) error {
+	l := left.(*object.Integer).Value
+	r := right.(*object.Integer).Value
 	var result int64
 
 	switch op {
@@ -100,6 +104,64 @@ func (vm *VM) executeBinaryIntegerOperation(op code.Opcode, l int64, r int64) er
 	}
 
 	return vm.push(&object.Integer{Value: result})
+}
+
+func (vm *VM) executeComparisonOperation(op code.Opcode) error {
+	r := vm.pop()
+	l := vm.pop()
+
+	if l.Type() == object.INTEGER_OBJ && r.Type() == object.INTEGER_OBJ {
+		return vm.executeIntComparisonOperation(op, l, r)
+	}
+
+	if l.Type() == object.BOOLEAN_OBJ && r.Type() == object.BOOLEAN_OBJ {
+		return vm.executeBoolComparisonOperation(op, l, r)
+	}
+
+	return fmt.Errorf("unsupported types for comparison operation: %s %s", l.Type(), r.Type())
+}
+
+func (vm *VM) executeIntComparisonOperation(op code.Opcode, left, right object.Object) error {
+	l := left.(*object.Integer).Value
+	r := right.(*object.Integer).Value
+	var result object.Object
+
+	switch op {
+	case code.OpEqual:
+		result = nativeBoolToBooleanObject(l == r)
+	case code.OpNotEqual:
+		result = nativeBoolToBooleanObject(l != r)
+	case code.OpGreaterThan:
+		result = nativeBoolToBooleanObject(l > r)
+
+	default:
+		return fmt.Errorf("unsupported operator: %d", op)
+	}
+
+	return vm.push(result)
+}
+
+func (vm *VM) executeBoolComparisonOperation(op code.Opcode, l, r object.Object) error {
+	var result object.Object
+
+	switch op {
+	case code.OpEqual:
+		result = nativeBoolToBooleanObject(l == r)
+	case code.OpNotEqual:
+		result = nativeBoolToBooleanObject(l != r)
+
+	default:
+		return fmt.Errorf("unsupported operator: %d", op)
+	}
+
+	return vm.push(result)
+}
+
+func nativeBoolToBooleanObject(b bool) *object.Boolean {
+	if b {
+		return True
+	}
+	return False
 }
 
 func (vm *VM) push(o object.Object) error {
